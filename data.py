fc345b84b5bea908afe6f34e83c89173431951bc
thearonn <58434647+thearonn@users.noreply.github.com>
2021-06-07 10:06:40 +0200

    initial commit

---
+++ data.py
@@ -0,0 +1,17 @@
+import torch
+import numpy
+from torchvision import datasets, transforms
+
+def mnist():
+    # exchange with the real mnist dataset
+ 
+    # Define a transform to normalize the data
+    transform = transforms.Compose([transforms.ToTensor(),
+                                    transforms.Normalize((0.5,), (0.5,))])
+    # Download and load the training data
+    trainset = datasets.MNIST('./.pytorch/MNIST_data/', download=True, train=True, transform=transform)
+
+    # Download and load the test data
+    testset = datasets.MNIST('./.pytorch/MNIST_data/', download=True, train=False, transform=transform)
+    return trainset, testset
+
